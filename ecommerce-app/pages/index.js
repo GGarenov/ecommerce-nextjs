d50239781f67d59d9a868723b42307fa1e90eaf9
Georgiy Garenov <g_garenov@outlook.com>
2023-11-23 18:57:38 +0200

    Change of the structure and some of the pages were created.

---
+++ ecommerce-app/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Button } from "@/components/elements/Button";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Button variant="dark">This is button</Button>
+    </div>
+  );
+}
