630c4e6e659881b432c3a2f95181a826445211e3
Georgiy Garenov <g_garenov@outlook.com>
2023-11-23 21:02:52 +0200

    Finally I fixed it by removing <a> tag

--- ecommerce-app/components/navigation/NavLink.jsx
+++ ecommerce-app/components/navigation/NavLink.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 export const NavLink = ({ url, label }) => {
   return (
     <li className="border-b border-slate-900 mr-1">
-      <Link href={url}>
-        <a className="flex text-gray-600 hover:bg-slate-200 cursor-pointer transition-colors duration-300 m-2 p-5 rounded-lg">
-          {label}
-        </a>
+      <Link
+        href={url}
+        className="flex text-gray-600 hover:bg-slate-200 cursor-pointer transition-colors duration-300 m-2 p-5 rounded-lg"
+      >
+        {label}
       </Link>
     </li>
   );
