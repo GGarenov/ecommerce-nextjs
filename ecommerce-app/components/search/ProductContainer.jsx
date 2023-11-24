8fc6c00acdf94313caccb8c4d337333044560219
Georgiy Garenov <g_garenov@outlook.com>
2023-11-24 21:41:23 +0200

    Search page was updated. Product and Results container were created.

---
+++ ecommerce-app/components/search/ProductContainer.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const imageLoader = ({ src }) => {
+  return `/images/products/${src}`;
+};
+
+const ProductContainer = ({ name, imageUrl }) => {
+  return (
+    <div className="text-center rounded-30">
+      <div className="border rounded border-slate-300">
+        <Image
+          loader={imageLoader}
+          src={`${imageUrl}`}
+          alt="product image"
+          width={150}
+          height={300}
+          objectFit="cover"
+        />
+      </div>
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default ProductContainer;
