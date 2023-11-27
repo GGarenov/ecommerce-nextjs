5c6db67869fdbecb4e3f8773fe3ba89f407eb6f8
Georgiy Garenov <g_garenov@outlook.com>
2023-11-27 21:25:52 +0200

    Cart was updated and finished but there is an error which I will fix tomorrow.

--- ecommerce-app/pages/cart.js
+++ ecommerce-app/pages/cart.js
@@ -1,14 +1,27 @@
 import { useCart } from "@/context/cart-context";
 import { Button } from "@/components/elements/Button";
+import ProductContainer from "@/components/search/ProductContainer";
 
 const Cart = () => {
   const { state } = useCart();
-  if (!state.products || state.product.length == 0) {
+  if (!state.products || state.products.length == 0) {
     return (
       <div className="flex justify-center">
         <p>Your Cart is Empty!</p>
       </div>
     );
   }
-  return <div></div>;
+
+  return (
+    <div>
+      {state.products.map((product, index) => {
+        return <ProductContainer key={index} product={product} />;
+      })}
+      <div className="flex md:max-w-screen-lg mx-auto justify-end">
+        <Button>Checkout</Button>
+      </div>
+    </div>
+  );
 };
+
+export default Cart;
