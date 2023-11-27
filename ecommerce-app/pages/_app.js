03e3491ddc2ab3c5ed735a1d45311644412a5057
Georgiy Garenov <g_garenov@outlook.com>
2023-11-27 20:53:19 +0200

    _app.js was updated

--- ecommerce-app/pages/_app.js
+++ ecommerce-app/pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import { Header } from "../components/navigation/Header";
+import { CartProvider } from "@/context/cart-context";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CartProvider>
+        <Header />
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
+  );
 }
 
 export default MyApp;
